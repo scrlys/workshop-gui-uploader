983fb2c2263412cf9afb3054c96453bf04f86299
scrlys <scrlys@users.noreply.github.com>
2017-06-17 22:58:51 -0700

    Added dummy layout for update mod

--- gui_frames.cpp
+++ gui_frames.cpp
@@ -1,11 +1,16 @@
+#include "gui_frames.h"
+
+#include <map>
+#include <vector>
+
 #include <wx/wxprec.h>
+#include <wx/filepicker.h>
 
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
 
 #include "steam.h"
-#include "gui_frames.h"
 
 IMPLEMENT_APP(App)
 
@@ -14,6 +19,8 @@ BEGIN_EVENT_TABLE(MainFrame, wxFrame)
     EVT_BUTTON(BUTTON_Update, MainFrame::OnUpdateItem)
     EVT_MENU(wxID_EXIT, MainFrame::OnExit)
 END_EVENT_TABLE()
+
+std::map<std::string, std::string> languages;
 
 bool App::OnInit()
 {
@@ -24,8 +31,37 @@ bool App::OnInit()
         return false;
     }
 
+    languages = std::map<std::string, std::string>();
+    languages["English"] = "english";
+    languages["Arabic"] = "arabic";
+    languages["Bulgarian"] = "bulgarian";
+    languages["Chinese (Simplified)"] = "schinese";
+    languages["Chinese (Traditional)"] = "tchinese";
+    languages["Czech"] = "czech";
+    languages["Danish"] = "danish";
+    languages["Dutch"] = "dutch";
+    languages["Finnish"] = "finnish";
+    languages["French"] = "french";
+    languages["German"] = "german";
+    languages["Greek"] = "greek";
+    languages["Hungarian"] = "hungarian";
+    languages["Italian"] = "italian";
+    languages["Japanese"] = "japanese";
+    languages["Korean"] = "koreana";
+    languages["Norwegian"] = "norwegian";
+    languages["Polish"] = "polish";
+    languages["Portuguese"] = "portuguese";
+    languages["Portuguese-Brazil"] = "brazilian";
+    languages["Romanian"] = "romanian";
+    languages["Russian"] = "russian";
+    languages["Spanish"] = "spanish";
+    languages["Swedish"] = "swedish";
+    languages["Thai"] = "thai";
+    languages["Turkish"] = "turkish";
+    languages["Ukrainian"] = "ukrainian";
+
     MainFrame *main_frame = new MainFrame(GetAppIdFromFile());
-    main_frame->Show(TRUE);
+    main_frame->Show(true);
     SetTopWindow(main_frame);
 
     return true;
@@ -44,7 +80,10 @@ void MainFrame::OnCreateItem(wxCommandEvent& event)
 
 void MainFrame::OnUpdateItem(wxCommandEvent& event)
 {
-    wxMessageBox(wxT("Updated item"), wxT("Title"), wxICON_INFORMATION);
+    //wxMessageBox(wxT("Updated item"), wxT("Title"), wxICON_INFORMATION);
+    UpdateFrame *update_frame = new UpdateFrame();
+    update_frame->Show(true);
+    //SetTopWindow(update_frame);
 }
 
 MainFrame::MainFrame(AppId_t app) : wxFrame(NULL, wxID_ANY, "Steam Workshop Uploader"), m_uploader(app)
@@ -56,4 +95,103 @@ MainFrame::MainFrame(AppId_t app) : wxFrame(NULL, wxID_ANY, "Steam Workshop Uplo
     sizer->Add(m_create);
     sizer->Add(m_update);
     SetSizer(sizer);
+}
+
+void UpdateFrame::OnUpdateName(wxCommandEvent& event)
+{
+    std::cerr << "name\n";
+}
+
+void UpdateFrame::OnUpdateDescription(wxCommandEvent& event)
+{
+    std::cerr << "desc\n";
+}
+
+void UpdateFrame::OnUpdatePreviewImage(wxCommandEvent& event)
+{
+    std::cerr << "prevew\n";
+}
+
+void UpdateFrame::OnUpdateVisibility(wxCommandEvent& event)
+{
+    std::cerr << "visibility\n";
+}
+
+void UpdateFrame::OnUpdatePath(wxCommandEvent& event)
+{
+    std::cerr << "update\n";
+}
+
+void UpdateFrame::OnFinish(wxCommandEvent& event)
+{
+    std::cerr << "finish\n";
+}
+
+std::vector<wxString> get_keys(std::map<std::string, std::string> map)
+{
+    std::vector<wxString> result;
+
+    std::map<std::string, std::string>::iterator iterator;
+    for (iterator = languages.begin(); iterator != languages.end(); ++iterator) {
+        result.push_back(wxString(iterator->first));
+    }
+
+    return result;
+}
+
+UpdateFrame::UpdateFrame() : wxFrame(NULL, wxID_ANY, "Steam Workshop Uploader")
+{
+    m_name = new wxTextCtrl(this, TEXT_name);
+    m_description = new wxTextCtrl(this, TEXT_description);
+    m_preview_path = new wxTextCtrl(this, wxID_ANY);
+    m_preview_select = new wxButton(this, FILE_preview, "Browse...");
+    m_language = new wxComboBox(this, COMBO_language, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, &get_keys(languages)[0], wxCB_READONLY);
+    m_visibility = new wxComboBox(this, COMBO_visibility, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0, NULL, wxCB_READONLY);
+    m_path_path = new wxTextCtrl(this, wxID_ANY);
+    m_path_select = new wxButton(this, FILE_path, "Browse...");
+    m_finish = new wxButton(this, BUTTON_finish, "Finish");
+
+    wxBoxSizer *vertical = new wxBoxSizer(wxVERTICAL);
+
+    wxBoxSizer *name = new wxBoxSizer(wxHORIZONTAL);
+    name->Add(new wxStaticText(this, wxID_ANY, _T("Name:")));
+    name->Add(m_name);
+
+    wxBoxSizer *description = new wxBoxSizer(wxHORIZONTAL);
+    description->Add(new wxStaticText(this, wxID_ANY, _T("Description:")));
+    description->Add(m_description);
+
+    wxBoxSizer *preview_file = new wxBoxSizer(wxHORIZONTAL);
+    preview_file->Add(m_preview_path);
+    preview_file->Add(m_preview_select);
+
+    wxBoxSizer *preview = new wxBoxSizer(wxHORIZONTAL);
+    preview->Add(new wxStaticText(this, wxID_ANY, _T("Preview image:")));
+    preview->Add(preview_file);
+
+    wxBoxSizer *language = new wxBoxSizer(wxHORIZONTAL);
+    language->Add(new wxStaticText(this, wxID_ANY, _T("Language:")));
+    language->Add(m_language);
+
+    wxBoxSizer *path_file = new wxBoxSizer(wxHORIZONTAL);
+    path_file->Add(m_path_path);
+    path_file->Add(m_path_select);
+
+    wxBoxSizer *path = new wxBoxSizer(wxHORIZONTAL);
+    path->Add(new wxStaticText(this, wxID_ANY, _T("Path:")));
+    path->Add(path_file);
+
+    wxBoxSizer *visibility = new wxBoxSizer(wxHORIZONTAL);
+    visibility->Add(new wxStaticText(this, wxID_ANY, _T("Visibility:")));
+    visibility->Add(m_visibility);
+
+    vertical->Add(name);
+    vertical->Add(description);
+    vertical->Add(preview);
+    vertical->Add(language);
+    vertical->Add(visibility);
+    vertical->Add(path);
+    vertical->Add(m_finish);
+
+    SetSizer(vertical);
 }
--- gui_frames.h
+++ gui_frames.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <wx/wxprec.h>
+#include <wx/filepicker.h>
+#include <map>
 
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
@@ -12,6 +14,13 @@ enum
 {
     BUTTON_Create = wxID_HIGHEST + 1,
     BUTTON_Update,
+    TEXT_name,
+    TEXT_description,
+    FILE_preview,
+    COMBO_language,
+    COMBO_visibility,
+    FILE_path,
+    BUTTON_finish,
 };
 
 class App : public wxApp
@@ -39,20 +48,28 @@ public:
 
 class UpdateFrame : public wxFrame
 {
-private:
+public:
+//    WorkshopUploader m_uploader;
+
+    wxTextCtrl *m_name;
+    wxTextCtrl *m_description;
+    wxTextCtrl *m_preview_path;
+    wxButton *m_preview_select;
+    wxComboBox *m_language;
+    wxComboBox *m_visibility;
+    wxTextCtrl *m_path_path;
+    wxButton *m_path_select;
+    wxButton *m_finish;
+
     void OnUpdateName(wxCommandEvent& event);
     void OnUpdateDescription(wxCommandEvent& event);
     void OnUpdatePreviewImage(wxCommandEvent& event);
     void OnUpdateLanguage(wxCommandEvent& event);
     void OnUpdateVisibility(wxCommandEvent& event);
+    void OnUpdatePath(wxCommandEvent& event);
 
     void OnFinish(wxCommandEvent& event);
 
 public:
     UpdateFrame();
 };
-
-//BEGIN_EVENT_TABLE(MainFrame, wxFrame)
-//    EVT_BUTTON(BUTTON_Create, MainFrame::OnCreateItem)
-//    EVT_BUTTON(BUTTON_Update, MainFrame::OnUpdateItem)
-//END_EVENT_TABLE()
